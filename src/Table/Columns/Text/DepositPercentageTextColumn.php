59be8a7717f12a364ac2c5f76c2243bc3b454ca1
kid <kidiatoliny@akira-io.com>
2024-09-20 20:20:41 -0100

    feat: add DepositPercentageTextColumn class
    Introduced a new `DepositPercentageTextColumn` class in the `Akira\FilamentToolKit\Table\Columns\Text` namespace. This class creates a text column specifically for displaying deposit percentages. Additionally, updated the Portuguese translation file to include the new label 'Amount'.

--- src/Table/Columns/Text/DepositPercentageTextColumn.php
+++ src/Table/Columns/Text/DepositPercentageTextColumn.php
@@ -6,11 +6,11 @@ namespace Akira\FilamentToolKit\Table\Columns\Text;
 
 use Filament\Tables\Columns\TextColumn;
 
-final class DepositPercentage
+final class DepositPercentageTextColumn
 {
     public static function make(): TextColumn
     {
         return TextColumn::make('deposit_percentage')
-            ->label(__('Deposit Percentage'));
+            ->label(__('Deposit percentage'));
     }
 }
