e76a613d7537bfa60258c88066e119ecfe3b4a3f
kid <kidiatoliny@akira-io.com>
2024-09-23 21:10:36 -0100

    feat: add SummaryTextColumn class
    Introduce `SummaryTextColumn` class for the summary text column in tables. Updated Portuguese translations and JSON script to include 'Resumo'. This allows for consistent handling and display of summary text entries across the application.

---
+++ src/Table/Columns/Text/SummaryTextColumn.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akira\FilamentToolKit\Table\Columns\Text;
+
+use Filament\Tables\Columns\TextColumn;
+
+final class SummaryTextColumn
+{
+    public static function make(): TextColumn
+    {
+        return TextColumn::make('summary')
+            ->label(__('Summary'))
+            ->limit();
+    }
+}
