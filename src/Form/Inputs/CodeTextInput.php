bb21fba9e9128b8a65a3440c0a25c5360efb321d
kid <kidiatoliny@akira-io.com>
2024-11-02 01:01:48 -0100

    feat: add action to generate random code suffix
    Implemented a suffix action to input field enabling users to generate new random codes. Introduced methods for customizable code size and default value generation using Laravel's Str helper. Added icon support for the action button.

--- src/Form/Inputs/CodeTextInput.php
+++ src/Form/Inputs/CodeTextInput.php
@@ -4,16 +4,25 @@ declare(strict_types=1);
 
 namespace Akira\FilamentToolKit\Form\Inputs;
 
+use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\TextInput;
+use Illuminate\Support\Str;
 
 final class CodeTextInput
 {
-    public static function make(): TextInput
+    public static function make($length = 8): TextInput
     {
         return TextInput::make('code')
             ->label(__('Code'))
             ->required()
             ->maxLength(191)
+            ->default(fn () => Str::random($length))
+            ->suffixAction(
+                Action::make('generateNewCode')
+                    ->iconButton()
+                    ->icon('heroicon-o-arrow-path-rounded-square')
+                    ->action(fn (TextInput $component) => $component->state(fn () => Str::random($length)))
+            )
             ->unique(ignoreRecord: true);
     }
 }
