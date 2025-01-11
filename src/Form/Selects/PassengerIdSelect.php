5fd740b7152d4a496d26db4632b1c40f19afa019
kid <kidiatoliny@akira-io.com>
2025-01-11 02:09:11 -0100

    feat: add PassengerIdSelect component for reusable passenger selection
    This commit introduces a new `PassengerIdSelect` class to streamline passenger selection with a searchable and preloaded dropdown. Additionally, translations for "Passenger" have been added in `pt_PT.json` and `selects.json` to support localization.

---
+++ src/Form/Selects/PassengerIdSelect.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akira\FilamentToolKit\Form\Selects;
+
+use Filament\Forms\Components\Select;
+
+final class PassengerIdSelect
+{
+    public static function make(): Select
+    {
+        return Select::make('passenger_id')
+            ->searchable()
+            ->preload()
+            ->label(__('Passenger'));
+    }
+}
